d7af80144f8e96fa867403564b2710621c797e38
Gabriel Andrés Mercado Rivera <gabriel.mercado@uao.edu.co>
2020-12-02 22:22:49 -0500

    correcion de error de direccionamiento

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
-import Login from './components/login/login'
+import Home from './components/home/Home'
 
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      <Home></Home>
     </div>
   );
 }
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './login.css'
+import './home.css'
 import Card from '../Card/card'
 
 
